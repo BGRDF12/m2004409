7528f3214faa246df22f079d5dc039a8044fd4bc
BGRDF12 <133920107+BGRDF12@users.noreply.github.com>
2024-05-08 12:12:54 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+    <title>A AND M ESTATES</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<div class="banner">
+    <div class="navbar">
+        <h1><a href="index.html">A&M ESTATES</a></h1>
+        <ul>
+            <li><a href="buy.html">BUY</a></li>
+            <li><a href="rent.html">RENT</a></li>
+            <li><a href="sell.html">SELL</a></li>
+            <li><a href="contact.html">CONTACT US</a></li>
+            <li><a href="login.html">LOGIN</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="login">
+    <form action="">
+        <h2>LOGIN</h2>
+        <div class="input-box">
+            <input type="text" placeholder="EMAIL ADDRESS" required>
+        </div>
+        <div class="input-box">
+            <input type="password" placeholder="PASSWORD" required>
+        </div>
+        <div class"forgot-password">
+            <a href="#">FORGOT PASSWORD?</a>
+        </div>
+        
+        <button type="submit" class="LOGIN">LOGIN</button>
+        
+        <div class="register-link">
+            <p>DON'T HAVE AN ACCOUNT?</p> <a href="#"> HERE</a>
+        </div>
+    </form>
+</div>
+
+<div class="footer">
+        <div class="socialIcons">
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+            <a href=""><i class="fa-brands fa-google-plus"></i></a>
+            <a href=""><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        <div class="footerNav">
+            <ul>
+                <li><a href="#">News</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact US</a></li>
+            </ul>
+        </div>
+    </div>
+    
+</body>
+</html>
